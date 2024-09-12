64ee76f38a1d2768d4394df70fc4ace7312c6c6d
cielotds <tadascielo01@gmail.com>
2024-09-12 15:22:30 +0800

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,8 @@
+class Activity {
+    public static void main(String[] args){
+       Dog dog = new Dog();
+       dog.name = "Oreo";
+
+       System.out.println(dog.showName());
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,11 @@
+class Dog {
+  String name;
+  boolean isMale;
+  int numberPuppies;
+  String mypuppies[] = new String[3];
+
+  public String showName() {
+    return "Name of the dog" + name;
+  }
+
+  }
